f617fc29fd16a42994ee94489422feeb0892e300
Bhavyesh Desai <bhvdsi+github.bhvdsi@gmail.com>
2019-10-26 18:30:27 +0530

    add comments

--- LoggedInScreen.java
+++ LoggedInScreen.java
@@ -14,15 +14,19 @@ class LoggedInScreen extends JFrame
     private JButton addWalletAmountButton;
     public LoggedInScreen(User user)
     {
+        //initialize a few variables
         this.user = user;
         this.setTitle(user.getName() + " - Cab Booking Screen");
         this.setSize(640, 480);
+        //create the panel
         thePanel = new JPanel(null);
+        //label to display user info
         userInfo = new JLabel("<html>"
                                 + "Name: " + user.getName() + "<br>"
                                 + "Email ID: " + user.getEmailID() + "<br>"
                                 + "Phone Number: " + user.getPhoneNumber() + "</html>");
         userInfo.setBounds(0, 0, 320, 50);
+        //label to display user balance
         if(user.getWalletBalance() >= 300)
             userWalletBalance = new JLabel("<html>Wallet Balance: <font color=\"green\">"
                     + String.valueOf(user.getWalletBalance())
@@ -38,14 +42,17 @@ class LoggedInScreen extends JFrame
         cities.add("DEF");
         cities.add("GHI");
         //END REPLACE
+        //components to select source
         sourceLabel = new JLabel("<html><font color=\"blue\">Starting Point:</font></html>");
         sourceLabel.setBounds(0, 70, 320, 20);
         source = new JComboBox(cities.toArray());
         source.setBounds(0, 90, 320, 20);
+        //components to select destination
         destinationLabel = new JLabel("<html><font color=\"blue\">Ending Point:</font></html>");
         destinationLabel.setBounds(0, 110, 320, 20);
         destination = new JComboBox((cities.toArray()));
         destination.setBounds(0, 130, 320, 20);
+        //components to add money to wallet of user
         addWalletAmount = new JTextField(1);
         addWalletAmount.setBounds(0, 150, 320, 20);
         addWalletAmountButton = new JButton("Add Amount to Wallet");
@@ -71,8 +78,10 @@ class LoggedInScreen extends JFrame
             }
         });
         addWalletAmountButton.setBounds(0, 170, 320, 20);
+        //label to display errors
         errorLabel = new JLabel("");
         errorLabel.setBounds(0, 190, 320, 20);
+        //add panel components
         thePanel.add(userInfo);
         thePanel.add(userWalletBalance);
         thePanel.add(sourceLabel);
